707a07bf739a1f1e455c82a8cf5ae7859f8a73dd
HAKN1999 <hafizdkurniawan700@gmail.com>
2022-11-07 21:04:50 +0700

    chore: add helpers

---
+++ helper/helper.go
@@ -0,0 +1,34 @@
+package helper
+
+import (
+	"github.com/go-playground/validator/v10"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func GeneratePasswordHash(password string) (string, error) {
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+	if err != nil {
+		return string(passwordHash), err
+	}
+
+	return string(passwordHash), nil
+}
+
+func ComparePasswordHash(passwordHashed, passwrod string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(passwordHashed), []byte(passwrod))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func FormatValidationError(err error) []string {
+	errors := make([]string, 0)
+
+	for _, e := range err.(validator.ValidationErrors) {
+		errors = append(errors, e.Error())
+	}
+
+	return errors
+}
